4a5674e85218985a321150da010efa5cc7051d03
abhayjha062 <abhayjha062@gmail.com>
2022-02-23 02:06:35 +0530

    netlify deployment

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -3,26 +3,28 @@ import React from 'react';
 const Matrix = (e) => {
     
   const toggleRed = (e) => {
-      //console.log(e.target.parentElement.children);
       let count = 0;
       let arr = [];
       let length = e.target.parentElement.children.length;
-      e.target.className = 'grid-item-change';
-      //console.log(e.target.parentElement.children[0].className);
+
       for(let i = 0; i< length; i++) {
-        if(e.target.parentElement.children[i].className == 'grid-item-change') {
+        if(e.target.parentElement.children[i].className === 'grid-item-change') {
             count ++;
             arr.push(i);
         }
-
-        if(count > 2) {
-            e.target.parentElement.children[i].className = 'grid-item';
-
-        }
       }
-      
 
-  }
+      if(count > 1) {
+        let j = arr[0];
+        e.target.parentElement.children[j].className = 'grid-item';
+        e.target.className = 'grid-item-change';
+        arr.shift();
+      } else {
+        e.target.className = 'grid-item-change';
+      }
+
+    }
+
   return (
       <div className="grid-container" onClick={toggleRed}>
         <div className="grid-item"></div>
